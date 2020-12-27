8fa71e0c1024af4ed3382bb5075579fef47fc780
proactive-callee-1012 <shaemc22@gmail.com>
2020-12-27 00:52:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ function currentLine(katzDeliLine){
   for (let i = 0; i < katzDeliLine.length; i++) {
     line.push(` `+ [i + 1] +`. `  + katzDeliLine[i])
     line = line.slice(0, -2);
-    return(`The line is currently:` {line});}
+    return(`The line is currently: {line});}
     
   if (katzDeliLine.length===0) {
     return "The line is currently empty.";
