d65e999d467a1787635919e6711522b4838782e9
proactive-callee-1012 <shaemc22@gmail.com>
2020-12-27 00:42:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -20,8 +20,9 @@ function nowServing(katzDeliLine){
 function currentLine(katzDeliLine){
 
   for (let i = 0; i < katzDeliLine.length; i++) {
-    line.push(` `+[i+1]+`. `  + katzDeliLine[i])
+    line.push(` `[i+1]`. `  + katzDeliLine[i])
     return(`The line is currently:` + line);}
+  
   if (katzDeliLine.length===0) {
     return "The line is currently empty.";
 }
